87a34b1b42d21f6d12cfdb87f6c43751f2ab9901
Cezary Klauza <cezary10klauza@gmail.com>
2024-12-10 21:50:20 +0100

    init

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,3 @@
-const nextConfig = {
-  experimental: {
-    turbo: {
-      rules: {
-        test: ["/.worker.(js|ts)$/"],
-        use: {
-          loaders: ["worker-loader"],
-        },
-      },
-    },
-  },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.worker\.(js|ts)$/,
-      use: { loader: "worker-loader" },
-    });
-
-    return config;
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
